65e10f68179adc997d717f045286d40aaa8d295a
Jeroen Rinzema <post@jeroen.no>
2021-09-12 23:26:56 +0200

    feat(examples): included a basic TLS connection example

---
+++ examples/tls/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"log"
+
+	wire "github.com/jeroenrinzema/psql-wire"
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	cert, err := tls.LoadX509KeyPair("./psql.crt", "./psql.key")
+	if err != nil {
+		return err
+	}
+
+	server, err := wire.NewServer("127.0.0.1:5432", wire.HandlerFunc(handle))
+	if err != nil {
+		return err
+	}
+
+	log.Println("PostgreSQL server is up and running at [127.0.0.1:5432]")
+	server.Certificates = []tls.Certificate{cert}
+	return server.ListenAndServe()
+}
+
+func handle(ctx context.Context, query string, writer wire.DataWriter) error {
+	return writer.Complete("OK")
+}
