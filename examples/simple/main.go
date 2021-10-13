6e3fdd6cc4c4ceb26795d54579411d9f1aeaecae
Jeroen Rinzema <post@jeroen.no>
2021-10-13 13:01:57 +0200

    feat: write some data inside the simple example

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	wire "github.com/jeroenrinzema/psql-wire"
+	"github.com/lib/pq/oid"
 )
 
 func main() {
@@ -12,7 +13,35 @@ func main() {
 	wire.ListenAndServe("127.0.0.1:5432", handle)
 }
 
+var table = wire.Columns{
+	{
+		Table:  0,
+		Name:   "name",
+		Oid:    oid.T_text,
+		Width:  256,
+		Format: wire.TextFormat,
+	},
+	{
+		Table:  0,
+		Name:   "member",
+		Oid:    oid.T_bool,
+		Width:  1,
+		Format: wire.TextFormat,
+	},
+	{
+		Table:  0,
+		Name:   "age",
+		Oid:    oid.T_int4,
+		Width:  1,
+		Format: wire.TextFormat,
+	},
+}
+
 func handle(ctx context.Context, query string, writer wire.DataWriter) error {
-	log.Println(query)
+	log.Println("incoming SQL query:", query)
+
+	writer.Define(table)
+	writer.Row([]interface{}{"John", true, 29})
+	writer.Row([]interface{}{"Marry", false, 21})
 	return writer.Complete("OK")
 }
