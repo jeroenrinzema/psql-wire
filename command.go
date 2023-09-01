c268d718efdf0188015c8325fd3e759c70a1b1f9
Jeroen Rinzema <jeroen@rinzema.dev>
2023-09-01 15:38:49 +0200

    fix: clear wg after handle command

--- command.go
+++ command.go
@@ -72,10 +72,9 @@ func (srv *Server) consumeCommands(ctx context.Context, conn net.Conn, reader *b
 		// NOTE: we increase the wait group by one in order to make sure that idle
 		// connections are not blocking a close.
 		srv.wg.Add(1)
-		defer srv.wg.Done()
-
 		srv.logger.Debug("incoming command", slog.Int("length", length), slog.String("type", string(t)))
 		err = srv.handleCommand(ctx, conn, t, reader, writer)
+		srv.wg.Done()
 		if errors.Is(err, io.EOF) {
 			return nil
 		}
